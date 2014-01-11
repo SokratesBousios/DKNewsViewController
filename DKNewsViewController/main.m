6d534d82484d7a96f57a75cb66ae0decd407aabb
Sokrates Bousios <sokratesbousios@Sokratess-MacBook-Pro.local>
2014-01-11 11:28:04 +0100

    Initial Commit

---
+++ DKNewsViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DKNewsViewController
+//
+//  Created by Sokrates Bousios on 11.01.14.
+//  Copyright (c) 2014 Sokrates Bousios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
